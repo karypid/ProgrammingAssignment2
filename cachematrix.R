7073f0d049f97d2df4cd2a917c846c6879d80053
karypid <karypid@yahoo.co.uk>
2015-03-18 01:28:58 +0000

    Update to more resilient interface.

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,66 @@
 ## Constructs an R object for storing a matrix and its inverse
 ## 
-## This function creates an R environment intended to store a
+## This function creates an R environment intended to store
 ## a matrix, along with its inverse matrix. It is meant to be
-## used as a parameter to the cacheSolve() function which will
-## transparently remember the inverse matrix and then reuse it
-## if it is ever needed again subsequently.
+## used as a parameter to the cacheSolve() function.
 ##
 ## The environment initially stores the parameter 'x' for the
-## matrix and the caller can retrieve it using the get() method
-## as follows:
+## matrix and the caller can retrieve it using the get() function
+## and also retrieve its inverse matrix with getinv() as follows:
 ##
 ## cm <- makeCacheMatrix(x)
 ## cm$get() # returns x
+## cm$getinv() # returns x's inverse
 ##
-## The inverse matrix is initially not stored. Use the setinv()
-## function to set the inverse matrix's value, which may then be
-## retrieved with getinv(), as follows:
+## The inverse is only calculated when getinv() is called for the
+## first time, at which point it will store the inverse matrix for
+## reuse. Subsequent calls will immediately return the cached result.
 ##
-## cm <- makeCacheMatrix(x)
-## cm$setinv(xinverse) # store xinverse in the environment
-## cm$getinv() # returns xinverse
+## It is noted that when working with large matrices, it may be
+## desirable to clear the cached value. To this end a clearinv()
+## function is provided that will discared the cached inverse matrix.
+## This will release the memory used for it, at the cost of having
+## getinv() recalculate it the next time it is called, which may be
+## preferable when you know will not be reusing the inverse matrix
+## for a while.
 ##
-## It is noted that setinv does not check whether the value to
-## be used is the actual inverse matrix, so the caller should not
-## abuse it to store arbitrary values as it may break other code
-## that relies it.
+## Please also note that it is possible to reset this environment
+## with another matrix value using the set() method. In this case
+## the cached value of the inverse matrix (if any) is discarded in
+## order for the getinv() method to recalculate it the next time it
+## is called.
 ##
-## Please note that it is possible to reset this environment with
-## another matrix value using the set() method. In this case the
-## cached value of the inverse matrix (if any) is discarded:
-##
+## Example usage:
+## 
 ## cm <- makeCacheMatrix(x)
 ## cm$get() # returns x
-## cm$setinv(xinverse) # store xinverse in the environment
-## cm$getinv() # returns xinverse
-## cm$set(y)
+## cm$getinv() # calculates and returns x's inverse
+## cm$set(y) # discards x and x's inverse, stores y
 ## cm$get() # returns y
-## cm$getinv() # returns NULL
+## cm$getinv() # calculates and returns y's inverse
+## cm$getinv() # returns y's inverse immediately
+## cm$clearinv() # discards the cached value for y's inverse
+## cm$getinv() # calculates and returns y's inverse
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   
+  get <- function() x
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinv <- function(y) inv <<- y
-  getinv <- function() inv
+  getinv <- function() {
+    if (is.null(inv)) {
+      message("recalc")
+      inv <<- solve(x)
+    }
+    inv
+  }
+  clearinv <- function() {
+    inv <<- NULL
+  }
   
-  list(set = set, get = get, setinv=setinv, getinv=getinv)
+  list(set = set, get = get, getinv=getinv, clearinv=clearinv)
 }
 
 ## This function takes an encapsulated matrix created by
@@ -59,16 +71,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ##
 ## Example usage:
 ##
-## m<-matrix(c(2,3,2,2), 2,2)
-## cacheSolve(makeCacheMatrix(m)) %*% m
+## m <- matrix(c(-1,-2,1,1), 2,2)
+## minv <- cacheSolve(makeCacheMatrix(m))
+## minv %*% m # identity matrix of size 2
 cacheSolve <- function(x, ...) {
-  inv = x$getinv()
-  
-  if (is.null(inv)) {
-    d = x$get()
-    inv <- solve(d)
-    x$setinv(inv)
-  }
-  
-  inv
+  x$getinv()
 }
